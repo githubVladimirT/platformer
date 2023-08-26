8d526eea59844c58e566c9dea0b99e73a26344f6
Vladimir Tolstov <93446392+githubVladimirT@users.noreply.github.com>
2023-08-26 14:06:14 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,43 +1,73 @@
 LEVEL_MAP = [
-	'                            XXXXXXXXXXXXXXXXXXXXXXXXXXX',
-	'                            XXXXXXXXXXXXXXXXXXXXXXXXXXX',
-	'         X                  XXXXXXXXXXXXXXXXXXXXXXXXXXX',
-	'       XXXX           XX    XXXXXXXXXXXXXXXXXXXXXXXXXXX',
-	'   P                        XXXXXXXXXXXXXXXXXXXXXXXXXXX',
-	'XXXXX         XX         XX XXXXXXXXXXXXXXXXXXXXXXXXXXX',
-	' XXXX       XX X            XXXXXXXXXXXXXXXXXXXXXXXXXXX',
-	' XX    X  XXXX    XX  XX          XXXXXXXXXXXXXXXXXXXXX',
-	'       X  XXXX    XX  XXX           XXXXXXXXXXXXXXXXXXX',
-	' X  XXXX  XXXXXX  XX  XXXX  XXXXXX                     ',
-	'XXXXXXXXX XXXXXX  XX  XXXX  XXXXXX                     ',
-	'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    'XXXX                  XXXXX     XXXXXXX                       X    X    X   X   X    X                 XX            XXXXXXXXXXXXXX',
+    'X         XX                               XXXXX               X  X      X X     X  X                                             X',
+    'X    X                                              XX XXXXX                                                    XX                X',
+    'X  X XXX                                                                                             XXXX     XXXXX   XX XXX      X',
+    'XXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   XXXXXXXXXXXXXXXXXXX  XXXXXXXXX XXXXXXX      X',
+	'X                            XXXXXXXXXXXX                                             XX       XXX      XX   XXXXXXX XXXXXXX      X',
+	'X                                        XXX   XX XXXX                              XXX         X     XXX   XXXXXXXX XXXXXXX      X',
+	'X         X    XXXX    X     X            XXXXX    X  XXX  XXXX                                XXX      XXX    XXXXX XXXXXXX      X',
+	'X       XXXX           XX    XXXXXXXXXXX                X              XXXX  XXXX                       X     XXXXXX XXXXXXX      X',
+	'X P                          XXX              XXX       X                         XX              X     XXXX     XXX XXXXXXX      X',
+	'XXXX X        XXXX        XX XXX             XXXXXXXXXXXX     XXXXXXX                 XXX       XXX   XXX XXX     XX XXXXXXX      X',
+	'X XX X       XX              XXXXXXXXXXXXX                                               XX   XX       XX      XXXXX XXXXXXX      X',
+	'X XX X  X  XX X     X    X         XXXXXXXXX                               XXX            XXXXX        XX     XXX    XXXXXXX      X',
+	'X    X  X  X  X    XX   XX           XXXXXXXXXXXXXXXXXXXXXXXX                                      XXXXXXXXX         XXXXXXX      X',
+	'X X  XXXX  X       XX  XXXX  X XXXX                                    XXX          XXXXX   X   XXXXXXXXX XXXX        XXXXXX      X',
+	'X  X X  XXX        XX  XXXX  X X XX                                XXX          XXXXXXX                XX   XXXX       XXXXX      X',
+	'X  X XXXXX XXXXXX  XXXXXXXXXXXX   XXXXXXXXXXXXXXXXXXXXXXX          X                                   XX     XXXX      XXXX      X',
+    'XX X    X      XXX XXX  X            XXX                 XX      XXX              XXXXXXXXXXXXXXXXXXXXXXX       XXXX     XXX      X',
+    'XX X      XXX  XX X         XXXXXXX       XXXXXX  XXXXXXXX XX         X         XXXXXXXXXXXXXXXXXXXXXXXXX                XXX      X',
+    'XXXX X   XX XX       XXXXXX X   XXX XXX XX   XXXXX  XX    XX XX     XXX  X    XXX                 XXX       XXXXX     XXXXXX      X',
+    'XXXX XX XX   XXXX  XXXXX    XX XX  XX  XX      XXXX   XX XXXX XX   XXX  XX  XXXXXXXXXXXXXXXXXXXXXXXXXXXXX     XXXXXX     XXX      X',
+    'XXXX XXXXXXXXXXXXXXXXXXXXX XXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX      XXXXXXXXXX     XXXXXX X   XXX      X',
+	'X  X XXX        XXXXX             XXXXX                                                                                  XXX      X',
+	'X    XX          XXX                                                                                                 XX  XXX      X',
+	'X    X     X      X                XXX       XXXX                                                                        XXX      X',
+	'X    X    XXX               XXXXXXXXXX                                                                                  XXXX      X',
+	'X X  X   XXXXX                                                                        XXXXXX                             XXX      X',
+	'XXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  X                     XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX       XXXXX      X',
+	'X XX XXXXXXXXXXXX                      XXX       X                XXX  XXXXX                                         X   XXX      X',
+	'X  X XXXXXXXXXXX                         XXXXX       X        XXXXX        X                                             XXX      X',
+	'X    XXXXXXXXXX                            XXX                XXX          XXXX                                    X     XXX      X',
+	'X    XXXXXXXXX                               X           X    X          XXX       X         X                           XXX      X',
+	'X     XXXXXXX                                X                X            XX                                    X       XXX      X',
+	'X X    XXXXX                                 XXXXXXX    XXXXXXX            X           X                                 XXX      X',
+	'X       XXX                                        XX  XX                                                      X         XXX      X',
+	'X  X     X                                           X                                                                   XXX      X',
+	'X                                                                                                            X           XXX      X',
+	'XX                                                                                                                       XXX      X',
+	'XXXX                                                                                                       X             XXX      X',
+	'XXXX XXX                                             XX                                                                  XXX      X',
+	'XXXX XXXXXX                                      X        XX                                             X               XXX      X',
+	'XXXX       XXX                                  XX        XXX                                                            XXX      X',
+	'XXXX  XXX      XX                              XXX        XXXX                                         X                 XXX      X',
+	'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX             XXXXX   XXXX',
 ]
 
 TILE_SIZE = 32
-SCREEN_WIDTH = 1366
-SCREEN_HEIGHT = 768
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
 
 # colors 
-BG_COLOR = '#060C17'
-PLAYER_COLOR = '#C4F7FF'
-TILE_COLOR = '#94D7F2'
+# BG_COLOR = '#060C17'
+BG_COLOR = '#000000'
+# PLAYER_COLOR = '#C4F7FF'
+PLAYER_COLOR = '#0000FF'
+TILE_COLOR = '#FFFFFF'
+# TILE_COLOR = '#94D7F2'
 
 # camera
-#CAMERA_BORDERS = {
-#	'left': 100,
-#	'right': 200,
-#	'top': 100,
-#	'bottom': 150
-#}
-
 CAMERA_BORDERS = {
-	'left': 50,
-	'right': 100,
-	'top': 50,
-	'bottom': 75
+	'left': 100,
+	'right': 200,
+	'top': 100,
+	'bottom': 150
 }
+
+# CAMERA_BORDERS = {
+# 	'left': 50,
+# 	'right': 100,
+# 	'top': 50,
+# 	'bottom': 75
+# }
